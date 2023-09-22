1e07e3e600df7140b7bf7760a0d2fb9df335f079
Rober Fabian Barreto <139162533+robersks@users.noreply.github.com>
2023-09-22 12:46:45 -0500

    feat: :sparkles: img pikachu actualizada

--- main.js
+++ main.js
@@ -9,14 +9,15 @@ let myPikachu = document.querySelector('#myPikachu');
 
 myPikachu.addEventListener('click',async() => {
     let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json()
+    let img =  res.sprites.other.dream_world.front_female;
+    let defaultImg = "https://i.gifer.com/Yhw9.gif"
     //console.log(res)
     Swal.fire({
-        title: 'Sweet!',
+        title: `${res.name}`,
         text: 'Modal with a custom image.',
-        imageUrl: `${res.sprites.front_default}`,
-        imageWidth: 400,
-        imageHeight: 200,
-        imageAlt: 'Custom image',
+        imageUrl: `${(img)? img:defaultImg}`,
+        imageWidth: "80%",
+        imageHeight: "80%",
       })
 })
 
