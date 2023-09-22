4ab9fc702fe7019c12ad307b3c17425c7e6abe30
Rober Fabian Barreto <139162533+robersks@users.noreply.github.com>
2023-09-22 13:33:16 -0500

    feat: :sparkles: se actualiza el codigo js

--- main.js
+++ main.js
@@ -3,21 +3,32 @@
 //     'That thing is still around?',
 //     'question'
 //   )
-  
 
-let myPikachu = document.querySelector('#myPikachu');
+let myPikachu = document.querySelector("#myPikachu");
 
-myPikachu.addEventListener('click',async() => {
-    let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json()
-    let img =  res.sprites.other.dream_world.front_female;
-    let defaultImg = "https://i.gifer.com/Yhw9.gif"
-    //console.log(res)
+myPikachu.addEventListener("click", async () => {
+    let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json();
+    let img = res.sprites.front_default;
+    // let defaultImg = colocar imagen por defecto
+
+
     Swal.fire({
         title: `${res.name}`,
         text: 'Modal with a custom image.',
-        imageUrl: `${(img)? img:defaultImg}`,
+        // imageUrl: `${res.sprites.front_default}`,
+        imageUrl: `${(img) ? img : defaultImg}`,
+        html: `
+            ${res.stats.map(data => `
+                <input 
+                    type="range" 
+                    id="uno" 
+                    value="${data.base_stat}">
+                <label for="uno"> 
+                    ${data.base_stat} 
+                    ${data.stat.name}</label><br>
+                    `).join("")}   
+        `,
         imageWidth: "80%",
         imageHeight: "80%",
-      })
-})
-
+    });
+});
