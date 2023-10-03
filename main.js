968d350d72b26dcf9be14b98ddda207b6afa0f05
Rober Fabian Barreto <139162533+robersks@users.noreply.github.com>
2023-10-02 22:24:13 -0500

    feat: :sparkles: Agregar funcion de cliquear en la tarjeta y abrir estadisticas

--- main.js
+++ main.js
@@ -1,66 +1,66 @@
-// Swal.fire(
-//     'The Internet?',
-//     'That thing is still around?',
-//     'question'
-//   )
+// Función para mostrar las estadísticas del Pokémon en un modal
+async function showPokemonStats(pokemonName) {
+    try {
+        // Hacer una solicitud a la API para obtener los datos del Pokémon
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+        let pokemonData = await response.json();
 
-let myPikachu = document.querySelector("#myPikachu");
-
-myPikachu.addEventListener("click", async () => {
-    let res = await (await fetch("https://pokeapi.co/api/v2/pokemon/pikachu")).json();
-    let img = res.sprites.front_default;
-    // let defaultImg = colocar imagen por defecto
-
-
-    Swal.fire({
-        title: `${res.name}`,
-        text: 'Modal with a custom image.',
-        // imageUrl: `${res.sprites.front_default}`,
-        imageUrl: `${(img) ? img : defaultImg}`,
-        html: `
-            ${res.stats.map(data => `
+        // Crear el contenido HTML para el modal
+        let img = pokemonData.sprites.front_default;
+        let defaultImg = ""; 
+        let modalContent = `
+            <h2>${pokemonData.name}</h2>
+            <img src="${img || defaultImg}" alt="${pokemonData.name}">
+            ${pokemonData.stats.map((data) => `
                 <input 
                     type="range" 
-                    id="uno" 
-                    value="${data.base_stat}">
-                <label for="uno"> 
+                    value="${data.base_stat}"
+                    disabled
+                >
+                <label>
                     ${data.base_stat} 
-                    ${data.stat.name}</label><br>
-                    `).join("")}   
-        `,
-        imageWidth: "80%",
-        imageHeight: "80%",
-    });
-}); 
+                    ${data.stat.name}
+                </label><br>
+            `).join("")}
+        `;
 
+        // Mostrar el modal
+        Swal.fire({
+            title: `${pokemonData.name}`,
+            text: 'Estadísticas del Pokémon',
+            html: modalContent,
+            imageWidth: "80%",
+            imageHeight: "80%",
+        });
+    } catch (error) {
+        console.error("Error al cargar estadísticas del Pokémon:", error);
+    }
+}
 
-//---------- traer los pokemones
-let loadButton = document.querySelector("#loadPokemon");
-let pokemonList = document.querySelector("#pokemonList");
-let apiUrl = "https://pokeapi.co/api/v2/pokemon";
-let offset = 0;
-let limit = 100; // Cantidad de Pokémon a cargar en cada página
-
-//loadButton.addEventListener("click", async () => {
+// Traer los Pokémon y mostrarlos en las tarjetas
 document.addEventListener("DOMContentLoaded", async () => {
-
     try {
+        let pokemonList = document.querySelector("#pokemonList");
+        let apiUrl = "https://pokeapi.co/api/v2/pokemon";
+        let offset = 0;
+        let limit = 100; // Cantidad de Pokémon a cargar en cada página
+
         // Hacer una solicitud a la API para obtener una página de Pokémon
         let response = await fetch(`${apiUrl}?offset=${offset}&limit=${limit}`);
         let data = await response.json();
 
         // Recorrer la lista de Pokémon y mostrarlos en el elemento pokemonList
-        data.results.forEach(async (pokemon) => {
-            let pokemonData = await (await fetch(pokemon.url)).json();
-            let pokemonName = pokemonData.name;
-            let pokemonImage = pokemonData.sprites.front_default;
-
+        data.results.forEach((pokemon) => {
+            let pokemonName = pokemon.name;
             let pokemonCard = document.createElement("div");
             pokemonCard.classList.add("pokemon-card");
             pokemonCard.innerHTML = `
                 <h2>${pokemonName}</h2>
-                <img src="${pokemonImage}" alt="${pokemonName}">
+                <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split("/")[6]}.png" alt="${pokemonName}">
             `;
+            // Agregar un evento de clic a la tarjeta de Pokémon para mostrar estadísticas
+            pokemonCard.addEventListener("click", () => showPokemonStats(pokemonName));
+
             pokemonList.appendChild(pokemonCard);
         });
 
