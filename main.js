ede2255c0ecf46269910695e941c6429c7c23dd5
Rober Fabian Barreto <139162533+robersks@users.noreply.github.com>
2023-10-02 20:15:07 -0500

    feat: :sparkles: mostar pokemones en la pagina

--- main.js
+++ main.js
@@ -31,4 +31,40 @@ myPikachu.addEventListener("click", async () => {
         imageWidth: "80%",
         imageHeight: "80%",
     });
+}); 
+
+
+//---------- traer los pokemones
+let loadButton = document.querySelector("#loadPokemon");
+let pokemonList = document.querySelector("#pokemonList");
+let apiUrl = "https://pokeapi.co/api/v2/pokemon";
+let offset = 0;
+let limit = 100; // Cantidad de Pokémon a cargar en cada página
+
+loadButton.addEventListener("click", async () => {
+    try {
+        // Hacer una solicitud a la API para obtener una página de Pokémon
+        let response = await fetch(`${apiUrl}?offset=${offset}&limit=${limit}`);
+        let data = await response.json();
+
+        // Recorrer la lista de Pokémon y mostrarlos en el elemento pokemonList
+        data.results.forEach(async (pokemon) => {
+            let pokemonData = await (await fetch(pokemon.url)).json();
+            let pokemonName = pokemonData.name;
+            let pokemonImage = pokemonData.sprites.front_default;
+
+            let pokemonCard = document.createElement("div");
+            pokemonCard.classList.add("pokemon-card");
+            pokemonCard.innerHTML = `
+                <h2>${pokemonName}</h2>
+                <img src="${pokemonImage}" alt="${pokemonName}">
+            `;
+            pokemonList.appendChild(pokemonCard);
+        });
+
+        // Incrementar el offset para la próxima página
+        offset += limit;
+    } catch (error) {
+        console.error("Error al cargar Pokémon:", error);
+    }
 });
